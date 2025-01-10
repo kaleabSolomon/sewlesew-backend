4f28c50f2332a0349c5d4e8e8617a791d34f8f70
kaleabSolomon <kaleabslmn@gmail.com>
2025-01-10 10:17:18 +0300

    add Prisma module and service with configuration support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
