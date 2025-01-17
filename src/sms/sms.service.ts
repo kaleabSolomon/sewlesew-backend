4c1017449148c8b8d40ef08e9a8eb5f3d0e74cb7
kaleabSolomon <kaleabslmn@gmail.com>
2025-01-17 15:02:35 +0300

    refactor sms service to use Twilio for sending SMS

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -1,36 +1,3 @@
-// import { Injectable } from '@nestjs/common';
-// import * as Sinch from 'sinch';
-
-// @Injectable()
-// export class SmsService {
-//   private sinchClient: any;
-
-//   constructor() {
-//     // Initialize Sinch client
-//     this.sinchClient = new Sinch({
-//       key: '3c209cd8-6943-4eb5-9101-d52f1a87ecbf', // Your Sinch API Key
-//       secret: 'w~hsAQa9HTMhHyCCEBqmc1O1aN', // Your Sinch API Secret
-//     });
-//   }
-
-//   // Send SMS method
-//   async sendSMS(to: string, message: string): Promise<any> {
-//     try {
-//       // Use the Sinch API to send the SMS
-//       const response = await this.sinchClient.sms.send({
-//         from: 'your-sinch-number', // The Sinch number you want to send from
-//         to: to, // The recipient's phone number
-//         body: message, // The SMS message body
-//       });
-
-//       return response;
-//     } catch (error) {
-//       console.error('Failed to send SMS', error);
-//       throw new Error('Failed to send SMS');
-//     }
-//   }
-// }
-
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Twilio } from 'twilio';
@@ -51,7 +18,7 @@ export class SmsService {
     try {
       await this.twilioClient.messages.create({
         body: message,
-        from: this.configService.get<string>('TWILIO_FROM_NUMBER_TEST_FAILURE'),
+        from: this.configService.get<string>('TWILIO_FROM_NUMBER_TEST_SUCCESS'),
         to,
       });
       console.log(`SMS sent successfully to ${to}`);
