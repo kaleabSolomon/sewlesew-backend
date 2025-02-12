aad9a0b1ef9408713e329616d4f5ccaef4274b0d
kaleabSolomon <kaleabslmn@gmail.com>
2025-02-12 21:36:17 +0300

    fix: simplify CORS configuration by removing redundant middleware

--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Explicitly allow your frontend
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
-  app.use(
-    (
-      req: any,
-      res: { header: (arg0: string, arg1: string) => void },
-      next: () => void,
-    ) => {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, OPTIONS',
-      );
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      next();
-    },
-  );
+
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(
     new ValidationPipe({
